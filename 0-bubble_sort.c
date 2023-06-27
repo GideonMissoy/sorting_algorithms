3332d0f00ae96cfd7668a564961e13113ab02a46
GideonMissoy <giddykips2030@gmail.com>
2023-06-27 16:07:58 -0700

    These files will be compiled with the functions.

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+
+/**
+ * bubble_sort - Sorts an array of ints in ascending order.
+ * @*array: the array to sort.
+ * @size: size of the array.
+ * Return: void.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int swap, tmp;
+	size_t p;
+
+	swap = 1;
+	while (swap)
+	{
+		swap = 0;
+		for (p = 1; p < size; p++)
+		{
+			if (array[p - 1] > array[p])
+			{
+				tmp = array[p - 1];
+				array[p - 1] = array[p];
+				array[p] = tmp;
+				swap = 1;
+				print_array(array, size);
+			}
+		}
+	}
+}
--- print_array.c
+++ print_array.c
@@ -6,7 +6,7 @@
  * @array: The array to be printed.
  * @size: Number of elements in @array
  */
-void print_array(const int *narray, size_t size)
+void print_array(const int *array, size_t size)
 {
 	size_t i;
 
