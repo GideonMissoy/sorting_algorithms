0a541a47d273e988231cd9176bd56e9a159999b5
GideonMissoy <giddykips2030@gmail.com>
2023-07-01 19:16:31 -0700

    3. Quick sort. This function sorts an array of integers in ascending order using the Quick sort algorithm.

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,66 +1,69 @@
 #include "sort.h"
 
-
 /**
- * swap - swaps 2 elements in the array
- * @a: first element
- * @b: second element
+ * prtn_schm - partition scheme for quicksort
+ * @array: Array to sort
+ * @low: lowest index of array
+ * @high: highest index of array
+ * Return: index of pivot
  */
-void swap(int *a, int*b)
+int prtn_schm(int *array, int low, int high)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
+	int p, q, r, s;
+	static int size, t;
 
-/**
- * partition - partitions the array around a pivot element
- * @array: array to sort
- * @low: low
- * @high: high
- */
-int partition(int *array, int low, int high)
-{
-	int pivot = array[high];
-	int i = low - 1;
-
-	for (int j = low; j < high; j++)
-	{
-		if (array[j] <= pivot)
+	if (t == 0)
+		size = high + 1, t++;
+	p = array[high];
+	q = low;
+	for (r = low; r < high; r++)
+		if (array[r] <= p)
 		{
-			i++;
-			swap(&array[i], &array[j]);
+			if (q != r)
+			{
+				s = array[q];
+				array[q] = array[r];
+				array[r] = s;
+				print_array(array, size);
+			}
+			q++;
 		}
+	if (q != high)
+	{
+		s = array[q];
+		array[q] = array[high];
+		array[high] = s;
+		print_array(array, size);
 	}
-	swap(&array[i + 1], &array[high]);
-	return i + 1;
+	return (q);
 }
 
 /**
- * quick_sort_recursive - performs the recursive steps
+ * quick_rec_sort - quicksort recursive function
  * @array: array to sort
- * @low: low
- * @high: high
+ * @low: lowest index
+ * @high: highest index
  */
-void quick_sort_recursive(int *array, int low, int high)
+void quick_rec_sort(int *array, int low, int high)
 {
+	int p;
+
 	if (low < high)
 	{
-		int pivot = partition(array, low, high);
-		quick_sort_recursive(array, low, pivot - 1);
-		quick_sort_recursive(array, pivot + 1, high);
+		p = prtn_schm(array, low, high);
+		quick_rec_sort(array, low, p- 1);
+		quick_rec_sort(array, p + 1, high);
 	}
 }
 
 /**
- * quick_sort - handle edge cases
+ * quick_sort - sorts array using quicksort algorithm.
  * @array: array to sort
- * @size: size of array to sort
+ * @size: size of the array to sort
  */
-void quick_sort(int  *array, size_t size)
+void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size <= 1)
+	if (array == NULL)
 		return;
-
-	quick_sort_recursive(array, 0, size - 1);
+	quick_rec_sort(array, 0, size - 1);
 }
